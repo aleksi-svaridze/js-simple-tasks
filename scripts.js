9a10e05c58e4c3e283ab64b466d5ef8ba045ce24
aleksi-svaridze <aleksi.svaridze08b@geolab.edu.ge>
2025-06-01 20:09:26 +0400

    second commit

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
-let result = 0;
+// let result = 0;
 
-let userPrompt = Number(prompt("Enter number"));
+// let userPrompt = Number(prompt("Enter number"));
 
-for (let i = 1; i <= 10; i++) {
-  result = userPrompt * i;
-  console.log(`${userPrompt} x ${i} = ${result}`);
+// for (let i = 1; i <= 10; i++) {
+//   result = userPrompt * i;
+//   console.log(`${userPrompt} x ${i} = ${result}`);
+// }
+
+for (let i = 20; i >= 10; i--) {
+  console.log(i);
 }
